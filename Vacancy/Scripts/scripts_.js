af5fc6dd0d458fed423a0cf96101bca3b374af84
Zakirovaad <84876294+Zakirovaad@users.noreply.github.com>
2021-05-28 07:16:58 +0300

    Add all my files

---
+++ Vacancy/Scripts/scripts_.js
@@ -0,0 +1,68 @@
+﻿var search_vac = {
+    id: 0,
+    name: "",
+    salary: 0,
+    organization: "",
+    contact_person: "",
+    phone_number: "",
+    empl_type: "",
+    description: ""
+};
+
+var global_vac = {
+    id: 0,
+    name: "",
+    salary: 0,
+    organization: "",
+    contact_person: "",
+    phone_number: "",
+    empl_type: "",
+    description: ""
+};
+
+function ParseVacancy(list) {
+    var vac = {
+        id: 0,
+        name: "",
+        salary: 0,
+        organization: "",
+        contact_person: "",
+        phone_number: "",
+        empl_type: "",
+        description: ""
+    };
+    if (list != null) {
+        vac.id = list.id;
+        if (list.name != null) {
+            vac.name = list.name;
+        }
+        if (list.salary != null) {
+            if (list.salary.from!=null) {
+                vac.salary = list.salary.from;
+            }
+            else vac.salary = list.salary.to;
+            
+        }
+        if (list.employer != null) {
+            vac.organization = list.employer.name;
+            vac.contact_person = list.employer.name;
+
+        }
+        if (list.contacts != null) {
+            vac.phone_number = list.contacts;
+        }
+        if (list.schedule != null) {
+            vac.empl_type = list.schedule.name;
+        }
+
+        if (list.employment != null) {
+            vac.empl_type = list.employment.name;
+        }
+
+        if (list.description != null) {
+            vac.description = list.description;
+        }
+
+    }
+    return vac;
+}
